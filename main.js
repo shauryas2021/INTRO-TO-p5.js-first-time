465afe4186d3fb26fe4d1b4f1867ee31ba9cb30e
shauryas2021 <85066104+shauryas2021@users.noreply.github.com>
2022-03-22 16:47:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+tint_color = "";
+}
+function draw(){
+    image(video, 0, 0, 640, 680);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('student_name.png');
+}
+function filterTint(){
+    tint_color = document.getElementById("color_name").value;
+}
